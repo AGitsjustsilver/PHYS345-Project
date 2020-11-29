20ab47d936463dae4e5b89ed0225fd9b5e09bf46
Alessandro Guaresti <alessguar@gmail.com>
2020-11-28 19:33:52 -0800

    Changes: 	- finished implementing encrypt

--- src/ByteRubiks.java
+++ src/ByteRubiks.java
@@ -3,16 +3,12 @@ import ij.process.*;
 
 public class ByteRubiks extends Rubiks {
 
-    private ByteProcessor bp = null;
-
     public ByteRubiks(){
         super();
     }
 
     public ByteRubiks(ImagePlus imp, ImageProcessor ip){
-        super.imp = imp;
-        super.ip = ip;
-        generateVectors();
+        super(imp, ip);
     }
 
     @Override
@@ -20,28 +16,63 @@ public class ByteRubiks extends Rubiks {
 //         Algorithm Steps
 //        * 1. Generate 2 different size arrays R and C
 //        * 2. Determine the max number of Iterations //
-        this.bp = new ByteProcessor(ip,true);
-        int iter = 0;
+        int iter = 0,
+            width = this.ip.getWidth(),
+            height = this.ip.getHeight();
+
         while (!checkIter(iter)) {
 //        * 3. Increment iterator by 1
             iter++;
 //        * 4. For each row of image:
-            for(int row=0; row < this.ip.getWidth(); row++) {
+            for(int row=0; row < width; row++) {
 //        *   a. calculate sum all values in row
+                if(arrSum(this.imgArr[row])%2==0) {
 //        *   b. take modulo 2
 //        *   c. if 0 -> right circular shift
+                    shift(Dir.RIGHT, row);
+                }else {
 //        *      else -> left circular shift
+                    shift(Dir.LEFT, row);
+                }
             }
 //        * 5. For each column of image:
-            for(int col=0; col < this.ip.getHeight(); col++) {
+            for(int col=0; col < height; col++) {
 //        *   a. calculate the sum of all column values
+                if(arrSum(this.imgArr[col]) % 2 == 0) {
 //        *   b. compute modulo 2
 //        *   c. if 0 -> up circular shift
-//        *      else -> down circular shift
+                    shift(Dir.UP,col);
+                }else {
+//        *      else ->down circular shift
+                    shift(Dir.DOWN,col);
+                }
             }
-//        * 6. Using array C apply XOR
-//        *   a. apply XOR to
-//        * 7.
+//        * 6. Using Vector Key C apply XOR to rows
+            for (int i = 0; i < width; i++) {
+                if (i%2!=0) {
+                    // a. Image[2i-1][j] (odd rows) get regular XOR of C[j]
+                    for (int j = 0; j < height; j++) {
+                        xorShift(i,j,false);
+                    }
+                }else {
+                    // b. Image[2i][j] (even rows) get left bit shifted C[j]
+                    for (int j = 0; j < height; j++) {
+                        xorRotShift(i,j,false);
+                    }
+                }
+            }
+//        * 7. Using Vector Key R apply XOR to columns
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    if(j%2!=0) {
+                        // a. Image[i][2j-1] (odd columns) get regular XOR of R[j]
+                        xorShift(i,j,true);
+                    }else {
+                        // b. Image[i][2j] (even columns) get left bit shifted R[j]
+                        xorRotShift(i,j,true);
+                    }
+                }
+            }
 //        * 8. if iter and iterMax are equal then its done else go again
         }
     }
--- src/Rubiks.java
+++ src/Rubiks.java
@@ -8,13 +8,33 @@ import java.security.SecureRandom;
  * */
 
 abstract class Rubiks {
-    private Vector<BigInteger> vectR;
-    private Vector<BigInteger> vectC;
+    private Vector<BigInteger> vectR; // true
+    private Vector<BigInteger> vectC; // false
     private int bitSize;
     private final int ITERMAX = 1;
 
+
     public ImagePlus imp;
     public ImageProcessor ip;
+    public int[][] imgArr;
+
+    public Rubiks(){
+        this.imp = new ImagePlus();
+        this.ip = null;
+        this.generateVectors();
+    }
+
+    public Rubiks(String path){
+        this.imp = new ImagePlus(path);
+        this.ip = imp.getProcessor();
+        this.generateVectors();
+    }
+
+    public Rubiks(ImagePlus imagePlus, ImageProcessor imageProcessor){
+        this.imp = imagePlus;
+        this.ip = imageProcessor;
+        this.generateVectors();
+    }
 
     //methods
     public void generateVectors(){
@@ -27,16 +47,66 @@ abstract class Rubiks {
         return (iter == ITERMAX);
     }
 
-    public Integer rowSum(){return null;}
-
-    public Integer colSum(){return null;}
-
-    private boolean shiftLeft(){
-        return false;
+    public int arrSum(int[] arr){
+        int sum = 0;
+        for (int i: arr) sum += i;
+        return sum;
     }
 
-    private boolean shiftRight(){
-        return false;
+    public void shift(Dir d, int index){
+        // col = UP/DOWN = true
+        // row = LEFT/RIGHT = false;
+        boolean direction = (d == Dir.UP)||(d == Dir.DOWN);
+        if(direction) {
+            this.colSwitch();
+            d = (d == Dir.UP)? Dir.LEFT: Dir.RIGHT;
+        }
+        int bound = this.imgArr[index].length-1;
+        if (d == Dir.RIGHT){
+            // shift ->
+            for (int row = 0; row < bound; row++){
+                swap(this.imgArr[index], row, row+1);
+            }
+            swap(this.imgArr[index], bound-1, bound);
+        }else {
+            // shift <-
+            for (int row = bound; row > 0; row--){
+                swap(this.imgArr[index], row-1, row);
+            }
+            swap(this.imgArr[index], 1, 0);
+        }
+        if(direction)
+            this.colSwitch();
+    }
+
+    public void xorShift(int i, int j, boolean key){
+        this.imgArr[i][j] = this.imgArr[i][j] ^ getKey(key, j);
+    }
+
+    public void xorRotShift(int i, int j, boolean key){
+        this.imgArr[i][j] = this.imgArr[i][j] ^ ( getKey(key, j) << 1);
+    }
+
+    private int getKey(boolean key, int pos){
+        return ((key)? this.vectR.get(pos): this.vectC.get(pos)).intValue();
+    }
+
+    private void swap(int[] arr, int indexA, int indexB){
+        int temp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = temp;
+    }
+
+    private void colSwitch(){
+        int rowLen = ip.getWidth(),
+                colLen = ip.getHeight();
+        int[][] temp = new int[rowLen][colLen];
+        for (int row = 0; row < rowLen; row++){
+            for (int col = 0; col < colLen; col++){
+                temp[col][row] = this.imgArr[row][col];
+            }
+        }
+        this.imgArr = temp;
     }
 
     private Vector<BigInteger> generateVector(int size){
@@ -60,4 +130,11 @@ abstract class Rubiks {
         }
         System.out.println(v.toString());
     }
+}
+
+enum Dir {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
 }
